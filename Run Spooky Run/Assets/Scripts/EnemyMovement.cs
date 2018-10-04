3c2af1778e3fec88fde4bd746072db27d6e547b2
RCElucidator7 <G00332826@gmit.ie>
2018-10-04 13:58:08 +0100

    Basic movement, jumping and fall to death added

---
+++ Run Spooky Run/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public int enemySpeed;
+	public int xDir;
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xDir, 0));
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (xDir, 0) * enemySpeed;
+		if(hit.distance < 1.7f){
+			Flip ();
+		}
+	}
+
+	void Flip (){
+		if (xDir > 0){
+			xDir = -1;
+		}
+		else {
+			xDir = 1;
+		}
+	}
+}
---
+++ Run Spooky Run/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour {
+
+    public int speed = 10;
+    private bool faceRight = false;
+    public int jump = 1250;
+    private float moveXPos;
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerMove ();
+	}
+
+    void PlayerMove()
+    {
+        //Controls
+        moveXPos = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //Player Direction
+        if(moveXPos < 0.0f && faceRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveXPos > 0.0f && faceRight == true)
+        {
+            FlipPlayer();
+        }
+
+        //Game Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveXPos * speed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //Code to jump
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump);
+
+    }
+
+    void FlipPlayer()
+    {
+        faceRight = !faceRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
